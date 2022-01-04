fcf8c0746305802bfc30ff337316e2c9c08f2923
James Cairns <james.cairns@outlook.com>
2022-01-04 08:46:03 +0000

    Fixed CORS error and redeployed.
    Fixed CORS error and redeployed.
    
    Has my ip for test purposes (not deployed)

--- GitHubInfoGrabberAPI/GitHubInfoGrabberAPI/Program.cs
+++ GitHubInfoGrabberAPI/GitHubInfoGrabberAPI/Program.cs
@@ -1,4 +1,21 @@
+var AllowMySite = "_allowMySite";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowMySite,
+        builder =>
+        {
+            builder.WithOrigins("http://thejamescairns.co.uk",
+                "http://www.thejamescairns.co.uk",
+                "https://thejamescairns.co.uk",
+                "https://www.thejamescairns.co.uk",
+                "http://127.0.0.1:5500")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
 
 // Add services to the container.
 
@@ -29,6 +46,8 @@ app.UseSwaggerUI(c =>
 });
 
 app.UseHttpsRedirection();
+
+app.UseCors(AllowMySite);
 
 app.UseAuthorization();
 
